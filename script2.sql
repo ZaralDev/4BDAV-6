1c516b4800d809f3cae7727d8e3ed5ad44eb303a
Nvolo10 <77746959+Nvolo10@users.noreply.github.com>
2022-06-01 11:49:43 +0200

    Update script2.sql

--- script2.sql
+++ script2.sql
@@ -1,15 +1,2 @@
 -- EXERCICE 1 :
-DECLARE
-    v_countries int := 25 ;
-    v_departments int := 27;
-    v_employee_id int := 107;
-    v_job_history int :=  10;
-    v_jobs int := 29;
-    v_locations int := 23;
-    v_regions int := 4;
-    somme int; 
-    
-BEGIN
-    somme := v_countries + v_departments + v_employee_id + v_job_history + v_jobs + v_locations + v_regions;
-    DBMS_OUTPUT.PUT_LINE('Il y a ' || somme || ' enregistrements dans toutes les tables du schéma HR');
-END;
+
