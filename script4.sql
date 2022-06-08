3aae9bddd2f366777d5504685c28caf2e86bca1c
Nvolo10 <77746959+Nvolo10@users.noreply.github.com>
2022-06-08 16:12:53 +0200

    Update script4.sql

--- script4.sql
+++ script4.sql
@@ -220,3 +220,49 @@
         return {min:v_min, max:v_max, number:v_number};
       };
 
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Hadoop
+ L'architecture Hadoop est composé des éléments suivants :
+    HDFS : le système de fichier distribué d’Apache Hadoop
+    YARN : le gestionnaire de ressources et des tâches dans un cluster Hadoop
+    MapReduce : un framework pour le traitement de grands ensembles de données en parallèle
+    Spark : un moteur d’analyse unifié pour le traitement de données à grande échelle
+    PIG, HIVE : Services de traitement de données à l’aide de requêtes du type SQL
+    HBase : système de gestion de base de données non-relationnelles distribué
+    Mahout, Spark MLlib : des services pour faire du Machine Learning
+    Apache Drill : Moteur de requête SQL sur Hadoop
+    Zookeeper : Pour la gestion de Cluster
+    Oozie : Système de planification de workflow pour gérer les jobs Hadoop
+    Flume, Sqoop : Services d’ingestion de données
+    Solr & Lucene : Pour la recherche et l’indexation
+    Ambari :  Mise à disposition, monitoring et maintenance de cluster
+ 
+ 
+-- Hive
+  L'architecture Hive est composé des éléments suivants :
+    metastore : est chargé du stockage des métadonnées pour chaque table c'est-à-dire qu'il enregistre par exemple les schémas ainsi que les localisations. 
+              Il inclut aussi des métadonnées de partitionnement afin d'aider le driver à suivre la distribution des bases de données au sein du cluster. 
+              En pratique, les données sont stockées à la manière d'un SGBD relationnel traditionnel.
+    driver : joue le rôle du contrôleur de processus recevant les instances HiveQL. Il lance l'exécution des instructions par l'intermédiaire de la création de
+            sessions et il contrôle la progression de l'exécution ainsi que le cycle de vie des processus. Il conserve les métadonnées nécessaires générées durant 
+            l'exécution des requêtes HiveQL. Le driver joue aussi le rôle de collecteur de données résultant des étapes Reduce.
+    compiler : réalise la compilation des requêtes HiveQL.
+    optimizer : réalise différentes transformations sur le plan d'exécution pour obtenir un DAG optimisé.
+    executor : après les phases de compilation et de d'optimisation, il exécute les tâches fournies par le DAG.
+    cLI, UI et Thrift Server : permettent à un utilisateur externe d’interagir avec Hive en soumettant des requêtes.
+
+
+-- Hbase
+  L'architecture Hbase est composé des éléments suivants :
+    HMaster : L’implémentation de Master Server dans HBase est HMaster. Il s’agit d’un processus dans lequel les régions sont affectées au serveur de région ainsi 
+              qu’aux opérations DDL (créer, supprimer une table). Il surveille toutes les instances de serveur de région présentes dans le cluster. Dans un 
+              environnement distribué, Master exécute plusieurs threads d’arrière-plan. HMaster possède de nombreuses fonctionnalités telles que le contrôle de 
+              l’équilibrage de charge, le basculement, etc. 
+    Serveur de région : Les tables HBase sont divisées horizontalement par plage de clés de ligne en régions. Les régions sont les éléments de base du cluster HBase 
+                        qui se compose de la distribution de tables et sont composées de familles de colonnes. Le serveur de région s’exécute sur HDFS DataNode qui est
+                        présent dans le cluster Hadoop. Les régions de Region Server sont responsables de plusieurs choses, comme la gestion, la gestion, l’exécution 
+                        ainsi que la lecture et l’écriture d’opérations HBase sur cet ensemble de régions. La taille par défaut d’une région est de 256 Mo. 
+    Zookeeper : C’est comme un coordinateur dans HBase. Il fournit des services tels que la gestion des informations de configuration, le nommage, la synchronisation 
+                distribuée, la notification de panne de serveur, etc. Les clients communiquent avec les serveurs de la région via zookeeper. 
