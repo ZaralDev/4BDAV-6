7da3e29e1bc3f0543a4bd38b1707074cf442f80b
Nvolo10 <77746959+Nvolo10@users.noreply.github.com>
2022-06-08 15:03:55 +0200

    Create script4.sql

---
+++ script4.sql
@@ -0,0 +1,35 @@
+-- Création des documents 
+{
+  "_id": 1
+  "Titre" : "Les Misérables",
+  "Réalisateur" : "Tom Hooper",
+  "Temps d'exécution" : 158  
+}
+
+{
+  "_id": 2,
+  "Titre": "Skyfall",
+  "Réalisateur": "Sam Mendes",
+  "Durée d'exécution": 137
+}
+
+{
+  "_id": 3,
+  "Titre": "Les grands hommes",
+  "Réalisateur": "Raoul Walsh"
+}
+
+-- Recherche dans la collection video_recording
+-- 1er document
+    {"_id": 1}
+    
+-- 2eme document
+    { "Titre": "Skyfall",}
+    
+-- 3eme document
+    {""Réalisateur": "Raoul Walsh"}
+    
+    
+    
+    
+-- DBLP
